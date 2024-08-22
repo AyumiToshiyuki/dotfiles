2e3ea00dbbe65aec9354ac486380db70bb347fe9
AyumiToshiyuki <ayumi.toshiyuki@gmail.com>
2024-08-22 23:59:52 +0200

    Removed clangd from nvim, too laggy

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
-					"clangd"
+--					"clangd"
 				}
 			})
 		end
@@ -20,9 +20,9 @@ return {
 		config = function()
 			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.clangd.setup({
-				capabilities = capabilities
-			})
+--			lspconfig.clangd.setup({
+--				capabilities = capabilities
+--			})
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 		end
