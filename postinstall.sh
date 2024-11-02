f09dce399f7839deaafe369d2fc79d537fe01151
Lilith Nephilim <85030278+AyumiToshiyuki@users.noreply.github.com>
2024-11-02 18:22:57 +0100

    fix --noconfirm argument

--- postinstall.sh
+++ postinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-yay -Syu --no-confirm
+yay -Syu --noconfirm
 
 # Apps
-yay -S --no-confirm ani-cli bat bucklespring discord eza feh firefox fzf htop keepassxc kitty lolcat mpv neovim oh-my-posh opentabletdriver pavucontrol picom qbittorrent ranger ripgrep sshfs tldr tmux tree unrar unzip wget yt-dlp zip zoxide zsh
+yay -S --noconfirm ani-cli bat bucklespring discord eza feh firefox fzf htop keepassxc kitty lolcat mpv neovim oh-my-posh opentabletdriver pavucontrol picom qbittorrent ranger ripgrep sshfs tldr tmux tree unrar unzip wget yt-dlp zip zoxide zsh
 
 # To replace if I ever switch to Wayland
-yay -S --no-confirm i3-wm polybar rofi ulauncher xorg xorg-xinit
+yay -S --noconfirm i3-wm polybar rofi ulauncher xorg xorg-xinit
 
 # Fonts
-yay -S --no-confirm noto-fonts-cjk noto-fonts-emoji nerd-fonts
+yay -S --noconfirm noto-fonts-cjk noto-fonts-emoji nerd-fonts
