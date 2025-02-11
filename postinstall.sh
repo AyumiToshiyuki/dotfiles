e36d338a58718cacb88e6ebfb2396059ed629156
Lilith Nephilim <ayumi.toshiyuki@gmail.com>
2025-02-11 11:02:43 +0100

    Moved picom to be with other xorg things

--- postinstall.sh
+++ postinstall.sh
@@ -3,10 +3,10 @@
 yay -Syu --noconfirm
 
 # Apps
-yay -S --noconfirm ani-cli bat bucklespring discord eza feh firefox fzf htop keepassxc kitty lolcat mpv neovim oh-my-posh-bin opentabletdriver pavucontrol picom qbittorrent ranger ripgrep sshfs tldr tmux tree unrar unzip wget yt-dlp zip zoxide zsh scrot xclip
+yay -S --noconfirm ani-cli bat bucklespring discord eza feh firefox fzf htop keepassxc kitty lolcat mpv neovim oh-my-posh-bin opentabletdriver pavucontrol qbittorrent ranger ripgrep sshfs tldr tmux tree unrar unzip wget yt-dlp zip zoxide zsh scrot xclip
 
 # To replace if I ever switch to Wayland
-yay -S --noconfirm i3-wm polybar rofi ulauncher xorg xorg-xinit
+yay -S --noconfirm i3-wm polybar rofi ulauncher xorg xorg-xinit picom 
 
 # Fonts
 yay -S --noconfirm noto-fonts-cjk noto-fonts-emoji nerd-fonts
